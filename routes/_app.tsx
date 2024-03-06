c66b83d5b2cd68fa8fa4f184a4ed8d1a31a3e617
Kyaw Zin Hein <kyawzinhein6@gmail.com>
2024-03-06 16:32:02 +0630

    Initial commit

---
+++ routes/_app.tsx
@@ -0,0 +1,22 @@
+import { type PageProps } from "$fresh/server.ts";
+export default function App({ Component }: PageProps) {
+  return (
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Random content from CM</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="/styles.css" />
+      </head>
+      <body>
+        <Component />
+      </body>
+    </html>
+  );
+}
---
+++ routes/index.tsx
@@ -0,0 +1,63 @@
+import { Handlers } from "$fresh/server.ts";
+import { parseHTML } from "npm:linkedom@0.16.8/worker";
+
+const getRandomContent = async () => {
+  const html = await fetch(`https://channelmyanmar.org?${Math.random()}`).then((
+    res,
+  ) => res.text());
+  const { document } = parseHTML(html);
+
+  const content = [...document.querySelectorAll("#slider1 .item")].map(
+    (el) => ({
+      title: el.querySelector(".ttps").textContent.trim(),
+      year: el.querySelector(".ytps")?.textContent?.trim(),
+      poster: el.querySelector("img").src,
+      link: el.querySelector("a").href,
+    }),
+  );
+
+  return content;
+};
+
+export const handler: Handlers = {
+  async GET(_req, ctx) {
+    const contentArr = (await Promise.all(
+      Array.from({ length: 4 }).map(() => getRandomContent()),
+    )).flat();
+
+    const contentSet = new Set(
+      contentArr.map((ct) => JSON.stringify(ct)),
+    ); // remove duplicates if there are any
+
+    const content = [];
+    contentSet.forEach((ct) => {
+      content.push(JSON.parse(ct));
+    });
+
+    return ctx.render(content);
+  },
+};
+
+export default function Home({ data: content }) {
+  return (
+    <div className="container">
+      <h1 className="text-center my-3">Random content from CM</h1>
+      <div className="row">
+        {content.map((ct) => (
+          <div className="col-6 col-md-3 col-lg-2 my-2">
+            <a href={ct.link} target="_blank">
+              <img
+                src={ct.poster ||
+                  "https://channelmyanmar.org/wp-content/uploads/2018/12/44196111_2314620771886952_165796934605340672_n-4.jpg"}
+                alt={ct.title}
+                height={180}
+                style="object-fit: contain; width: 100%;"
+              />
+              <p className="mt-2 text-center">{ct.title}</p>
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
